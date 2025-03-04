eb0d1f07678db17381af577ec26b613548ada9c1
askmrmaitra <askmrmaitra@gmail.com>
2025-03-04 15:03:45 +0530

    Add project files.

---
+++ EFCorewithCA/Program.cs
@@ -0,0 +1,95 @@
+﻿using EFCorewithCA.data;
+
+namespace EFCorewithCA
+{
+    internal class Program
+    {
+        //UI 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Understanding EF Core...");
+            Console.WriteLine("****************************");
+            Console.WriteLine("Saving Data to Table...");
+            int Status = SaveData();
+            Console.WriteLine(Status);
+            if (Status == 1)
+            {
+                Console.WriteLine("Data Saved...");
+                Console.WriteLine("Press Any Key to Display the Data");
+                Console.ReadKey();
+                Display();
+                Console.WriteLine("Let us update the Data...");
+                int ProductID = 1;
+                string ProductName = "HP Laptop";
+                int ProductPrice = 75000;
+                UpdateProduct(ProductID, ProductName, ProductPrice);
+                Console.WriteLine("Product updated....");
+                Display();
+                Console.WriteLine("Do you want to Remove the Product?");
+                Console.ReadKey();
+                DeleteProduct(ProductID);
+                Console.WriteLine("Product Removed...Press Any Key to terminate");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("OOPS!!! Data has not been saved...");
+            }
+        }
+        public static int SaveData()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                var newProduct = new Product { Name = "Laptop", Price = 50000 };
+                dbContext.Products.Add(newProduct);
+                int Status = dbContext.SaveChanges();
+                return Status;
+            }
+
+        }
+        public static void UpdateProduct(int productId, string newName, int newPrice)
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                var product = dbContext.Products.Find(productId);
+
+                if (product != null)
+                {
+                    product.Name = newName;
+                    product.Price = newPrice;
+
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+
+        public static void DeleteProduct(int productId)
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                var product = dbContext.Products.Find(productId);
+
+                if (product != null)
+                {
+                    dbContext.Products.Remove(product);
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+
+        public static void Display()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                var products = dbContext.Products.ToList(); // Retrieve all products
+
+                // Display the products
+                foreach (var product in products)
+                {
+                    Console.WriteLine($"Product ID: {product.ProductId}, Name: {product.Name}, Price: {product.Price}");
+                }
+            }
+
+        }
+    }
+}
---
+++ EFCorewithCA/data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCorewithCA.data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=TrainingDB;Integrated Security=True;TrustServerCertificate=true");
+        }
+       
+    }
+}
